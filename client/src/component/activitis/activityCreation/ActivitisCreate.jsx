fc987abae2cb9c814e26054295d602f82fb48639
Cesar Gabriel Londero <gabriellondero40@.com>
2023-04-22 09:33:44 -0300

    validation

---
+++ client/src/component/activitis/activityCreation/ActivitisCreate.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+const ActivitisCreate = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ActivitisCreate
---
+++ client/src/component/activitis/activityCreation/validation.js
@@ -0,0 +1,14 @@
+export const validation = (userData, errors) => {
+    const error = {...errors};
+    if (!userData.Nombre.trim()) error.Nombre = 'Debes completar este campo';
+    else error.Nombre = '';
+    if (!userData.Duracion) error.Duracion = 'Debes completar este campo';
+    else error.Duracion = '';
+    if (!userData.Dificultad) error.Dificultad = 'Debes completar este campo';
+    else error.Dificultad = '';
+    if (!userData.Temporada) error.Temporada = 'Debes completar este campo';
+    else error.Temporada = '';
+    if (!userData.countryId === []) error.countryId = 'Debes elegir un País';
+    else error.countryId = '';
+    return error;
+}
